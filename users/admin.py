eee6f1586f47143971222ecfb3b0e8546e9be65e
easin <easin562050@gmail.com>
2025-06-29 23:21:49 +0600

    models create done

--- users/admin.py
+++ users/admin.py
@@ -3,22 +3,22 @@ from django.contrib.auth.admin import UserAdmin
 from users.models import User
 
 class CustomUserAdmin(UserAdmin):
-    model = User
-    list_display = ('email', 'first_name', 'last_name', 'is_active')
-    list_filter = ('is_staff','is_active')
-    fieldsets = (
-        (None, {"fields": ('email', 'password')}),
-        ('personal_info', {'fields': ('first_name', 'last_name', 'address', 'phone')}),
-        ('permissions', {'fields': ('is_staff', 'is_superuser', 'groups', 'user_permissions')}),
-        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    model=User
+    list_display=('email','first_name','last_name','is_staff','is_active')
+    list_filter=('is_staff','is_active')
+    fieldsets=(
+        (None,{'fields':('email','password')}),
+        ('Personal_info',{'fields':('first_name','last_name','address')}),
+        ('Permissions',{'fields':('is_staff','is_superuser','groups','user_permissions')}),
+        ('Important dates',{'fields':('last_login','date_joined')})
     )
-    add_fieldsets = (
-        (None, {
-            "classes": ('wide',),
-            "fields": ('email', 'password1', 'password2', 'first_name', 'is_staff', 'is_superuser')}
-        ),
+    add_fieldsets=(
+        (None,{
+            'classes':('wide',),
+            'firelds':('email','password1''password2','is_staff','is_active')
+        })
     )
-    search_fields = ('email',)
-    ordering = ('email',)
+    search_fields=('email',)
+    ordering=('email',)
 
-admin.site.register(User, CustomUserAdmin)
+admin.site.register(User,CustomUserAdmin)
--- users/managers.py
+++ users/managers.py
@@ -1,9 +1,9 @@
-from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.models import BaseUserManager
 
 class CustomUserManager(BaseUserManager):
     def create_user(self,email,password=None,**extra_fields):
         if not email:
-            raise ValueError('This field is must be set')
+            raise ValueError('This field must be set')
         
         email=self.normalize_email(email)
         user=self.model(email=email,**extra_fields)
@@ -16,8 +16,11 @@ class CustomUserManager(BaseUserManager):
         extra_fields.setdefault('is_superuser',True)
 
         if not extra_fields.get('is_staff'):
-            raise ValueError('Superuser must have is_staff =True')
+            raise ValueError('Superuser must have is_staff=True.')
         if not extra_fields.get('is_superuser'):
-            raise ValueError('Superuser must have is_superuser =True')  
-        return self.create_user(email,password,**extra_fields)  
-    
+            raise ValueError('Super user must have is_superuser=True')
+
+        return self.create_user(email,password,**extra_fields)
+
+
+        

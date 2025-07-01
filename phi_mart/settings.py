3cf49b9feb807f91931a5196dd559c191ec77cf9
easin <easin562050@gmail.com>
2025-07-01 11:43:58 +0600

    database is ready with json

--- phi_mart/settings.py
+++ phi_mart/settings.py
@@ -36,15 +36,17 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    "django.contrib.staticfiles",
     'rest_framework',
     'users',
     'order',
     'product',
-    'api'
+    'api',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -52,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
 ]
 
 ROOT_URLCONF = 'phi_mart.urls'
@@ -84,6 +87,11 @@ DATABASES = {
     }
 }
 
+INTERNAL_IPS = [
+    # ...
+    "127.0.0.1",
+    # ...
+]
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
--- product/models.py
+++ product/models.py
@@ -10,13 +10,13 @@ class Category(models.Model):
 
 class Product(models.Model):
     name=models.CharField(max_length=200)
-    descrition=models.TextField(blank=True,null=True)
+    description=models.TextField(blank=True,null=True)
     price=models.DecimalField(max_digits=10,decimal_places=2)
     stock=models.IntegerField()
     image=models.ImageField(upload_to='products/image/',blank=True,null=True)
     category=models.ForeignKey(Category,on_delete=models.CASCADE,related_name='products')
-    created_at=models.DateField(auto_now_add=True)
-    updated_at=models.DateField(auto_now=True)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name

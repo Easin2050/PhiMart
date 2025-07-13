74c63ce6f34965922a885a368ee0ba25cd626f86
easin <easin562050@gmail.com>
2025-07-13 22:42:33 +0600

    api api-root path added

--- order/views.py
+++ order/views.py
@@ -1,27 +1,28 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin, DestroyModelMixin
 from rest_framework.viewsets import GenericViewSet, ModelViewSet
-from rest_framework.mixins import CreateModelMixin,DestroyModelMixin,RetrieveModelMixin
-from order.models import Cart,CartItem
-from order.serializer import CartSerializer,CartItemSerializer,AddCartItemSerializer
+from order.serializer import CartSerializer, CartItemSerializer, AddCartItemSerializer, UpdateCartItemSerializer
+from order.models import Cart, CartItem
 # Create your views here.
 
-class CartViewSet(CreateModelMixin,RetrieveModelMixin,DestroyModelMixin,GenericViewSet):
-    queryset=Cart.objects.all()
-    serializer_class=CartSerializer
+
+class CartViewSet(CreateModelMixin, RetrieveModelMixin, DestroyModelMixin, GenericViewSet):
+    queryset = Cart.objects.all()
+    serializer_class = CartSerializer
+
 
 class CartItemViewSet(ModelViewSet):
-    serializer_class=CartItemSerializer
+    http_method_names = ['get', 'post', 'patch', 'delete']
 
-    def get_serializer(self):
-        if self.request.method=='POST':
-            return AddCartItemSerializer()
+    def get_serializer_class(self):
+        if self.request.method == 'POST':
+            return AddCartItemSerializer
+        elif self.request.method == 'PATCH':
+            return UpdateCartItemSerializer
         return CartItemSerializer
 
     def get_serializer_context(self):
-        return{'cart_id': self.kwarsgs['cart_pk']}
+        return {'cart_id': self.kwargs['cart_pk']}
 
     def get_queryset(self):
         return CartItem.objects.filter(cart_id=self.kwargs['cart_pk'])
-
-
